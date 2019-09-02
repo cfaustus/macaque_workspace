682af3c3161313cfb165b6f3d77efe21499d8554
Christina Faust <christina.faust@gmail.com>
2019-09-02 12:19:06 +0100

    adding in a figure to look at virulent malarias

--- map_figures.R
+++ map_figures.R
@@ -7,7 +7,11 @@ library(maptools)
 library(scales)
 library(rgdal)
 library(sp)
-
+library(mapplots)
+library(maps)
+library(plyr)
+library(rgeos)
+library(raster)
 
 #########
 #getting shapefiles
@@ -26,10 +30,71 @@ library(sp)
 #            unique[i], driver="ESRI Shapefile",overwrite_layer=TRUE)
 # }
 
-### Phenotyp
+### Phenotype Map
+dat = read.csv("data/phenotype_ltmacaque_data.csv", header = T)
+head(dat)
+fascicularis_range=readOGR("data/Macaca fascicularis.shp")
+
+# c('A','Q','P','AQ','AP','AQP',
+#   'QP','AX','AQX','APX','QPX'), 
+col_pal = c('#2166ac','#fddbc7','#b2182b','#67a9cf','#ef8a62','#d1e5f0')
+#col_pal=c('#8c510a','#c7eae5','#01665e','#d8b365','#5ab4ac','#f6e8c3')
+#col_pal=c('#291620','#2A2F3F','#184A50','#28644A','#607738','#A78138','#E98465')
+# col_pal = c('white','gold3','#335B8E','gold',
+#             '#B7DBDB','steelblue3', 'steelblue4','khaki3',
+#             '#B5B867','#6CA18F','olivedrab4')
+# map ####
+map("world", xlim=c(93,130), ylim=c(-11.5,23), 
+    col="gray90", fill=TRUE)
+
+plot(fascicularis_range, add=TRUE, col=alpha("grey10",0.3), border=FALSE)
+#points(dat$long, dat$lat, pch=19, col="red", cex=0.5,)  #plot sites to check locations
+# phenotypes A	AQ	Q	P	AP	QP	AQP	AX	AQX	APX	QPX
+for(i in 1:nrow(dat)) {
+  add.pie(z=c(dat$A[i],dat$Q[i], dat$AQ[i], dat$AX[i], dat$AQX[i],
+              (dat$P[i]+dat$AP[i]+dat$AQP[i]+dat$QP[i]+dat$APX[i]+dat$QPX[i])), 
+          radius=sqrt(dat$total[i])/8.5, 
+          x=dat$long[i], y=dat$lat[i], lty = 0,
+          col= col_pal,
+          #c(alpha(col_pal, 0.6))
+          labels="")
+}
+# legend ####
+legend(123, 4.3, 
+       title = 'phenotypes ',
+       c('A','Q','AQ','AX','AQX',
+         'other'), 
+       fill = col_pal,
+       bty = 'o', box.col = 'white',cex = 0.7,
+       bg = alpha('grey80',0.75))
+segments(97.5,-1,97.5,-10, lwd=2)
+segments(97.5,-1,97.8,-1, lwd=2)
+segments(97.5,-10,97.8,-10, lwd=2)
+
+### Virulent Map
+cambodia = readOGR("data/KHM_adm/KHM_adm0.shp") #cambodia
+singapore = readOGR("data/SGP_adm/SGP_adm0.shp")
+thailand = readOGR("data/THA_adm/THA_adm0.shp")
+indonesia = readOGR("data/IDN_adm/IDN_adm1.shp")
+bali = indonesia[indonesia$NAME_1 == 'Bali',]
+java_names = c('Jakarta Raya','Jawa Tengah', 'Jawa Barat', 'Jawa Timur')
+java = indonesia[indonesia$NAME_1 %in% java_names,]
+malaysia = readOGR('data/MYS_adm/MYS_adm1.shp')
+borneo_names = c('Sabah', 'Sarawak')
+pen_malay = malaysia[!malaysia$NAME_1 %in% borneo_names,]
+
+#intersection
+vir_countries = bind(cambodia, singapore, bali, thailand, java, pen_malay)
+fascicularis_range=readOGR("data/Macaca fascicularis.shp")
+monkey_vir = intersect(fascicularis_range,vir_countries)
 
 
+# c('A','Q','P','AQ','AP','AQP',
+#   'QP','AX','AQX','APX','QPX'), 
+col_pal = c('#2166ac','#fddbc7','#b2182b','#67a9cf','#ef8a62','#d1e5f0')
 
+
+mosaicplot(x[,2:4], main = 'Proportions')
 
 ### Range overlap
 
