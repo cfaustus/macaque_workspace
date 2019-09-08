bf01d429c3ce3b21b76c5d53b2c3e107c1435f88
Christina Faust <christina.faust@gmail.com>
2019-09-08 14:35:21 +0100

    changing size of legend

--- map_figures.R
+++ map_figures.R
@@ -60,17 +60,19 @@ for(i in 1:nrow(dat)) {
           labels="")
 }
 # legend ####
-legend(123, 4.3, 
+legend(121, 4.3, 
        title = 'phenotypes ',
        c('A','Q','AQ','AX','AQX',
          'other'), 
        fill = col_pal,
-       bty = 'o', box.col = 'white',cex = 1.1,
+       bty = 'o', box.col = 'white',cex = 0.8,
        bg = alpha('grey80',0.75))
 segments(97.5,-1,97.5,-10, lwd=2)
 segments(97.5,-1,97.8,-1, lwd=2)
 segments(97.5,-10,97.8,-10, lwd=2)
+#export as 5x5 PDF
 
+###########
 ### Virulent Map
 cambodia = readOGR("data/KHM_adm/KHM_adm0.shp") #cambodia
 singapore = readOGR("data/SGP_adm/SGP_adm0.shp")
@@ -86,12 +88,11 @@ pen_malay = malaysia[!malaysia$NAME_1 %in% borneo_names,]
 
 #intersection
 vir_countries = bind(cambodia, singapore, bali, thailand, java, pen_malay)
-fascicularis_range=readOGR("data/Macaca fascicularis.shp")
+fascicularis_range=readOGR("data/macaques/Macaca fascicularis.shp")
 monkey_vir = intersect(fascicularis_range,vir_countries)
 
 map("world", xlim=c(93,130), ylim=c(-11.5,23), 
     col="gray90", fill=TRUE)
-
 plot(fascicularis_range, add=TRUE, col=alpha("grey10",0.3), border=FALSE)
 plot(monkey_vir, add=TRUE, col=alpha("darkred",0.5), border=FALSE)
 legend(110, 16.3, 
@@ -107,7 +108,7 @@ mal_tab = malaria[,4:7]
 dimnames(mal_tab)[[1]] <- c("Cambodia",'Thailand','PeninsularMalaysia', 
                             'Singapore', "Java", "Bali")
 mosaicplot(mal_tab, main ='',
-           col= c('white','gray80', 'gray50', 'darkred'), 
+           col= c('white','gray80', 'darkred', 'gray50'), 
            las = 4)
 
 ### Range overlap
@@ -120,7 +121,8 @@ nemestrina_range=readOGR("data/macaques/Macaca nemestrina.shp")
 arctoides_range=readOGR("data/macaques/Macaca arctoides.shp")
 
 #SEAsia map
-map('world', xlim=c(90,150),ylim=c(-12,30))
+map("world", xlim=c(85,145), ylim=c(-11.5,30), 
+    col="gray90", fill=TRUE)
 #map.axes() #adds axis of lat/long to map
 
 plot(fascicularis_range, add=TRUE, col=alpha("red",0.5), border=FALSE)
